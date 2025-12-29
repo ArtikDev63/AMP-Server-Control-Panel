d2ee5cd86919744fa91d4cfad93f7fa9d2e11d3a
ArtikDev63 <amoyarodriguez99@gmail.com>
2025-12-29 19:43:14 +0100

    Successfully established login input to create the login system with database and token usage

--- AMP-Back/routes/login.js
+++ AMP-Back/routes/login.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const { json } = require("express");
+const { jsonResponse} = require("../lib/jsonResponse")
 
-router.get("/", (req, res) => {
-    res.send("login");
+router.post("/", (req, res) => {
+    const {username, password} = req.body;
+    
+    if (!username || !password){
+        return res.status(400).json(
+            jsonResponse(400, {
+                error: "Fields are required",
+            })
+        )
+    }
+    //Aunteticar usuario
+    const accessToken = "access_token"
+    const refreshToken = "refresh_token"
+    const user = {
+        id:"1",
+        name:"Alvaro Moya",
+        username:"XXXXX",
+    }
+
+    res.status(200).json(jsonResponse(200, { user, accessToken, refreshToken}))
 })
 
 module.exports = router;
--- AMP-Front/src/routes/LoginForm.js
+++ AMP-Front/src/routes/LoginForm.js
@@ -2,12 +2,46 @@ import { FaUser, FaLock, FaCube } from 'react-icons/fa';
 import "./css/LoginForm.css"
 import { useState } from 'react';
 import {useAuth} from "../auth/AuthProvider"
+import { API_URL } from '../auth/constants';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginForm(){
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [errorResponse, setErrorResponse] = useState("")
     const auth = useAuth()
+    const goTo = useNavigate()
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`${API_URL}/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            });
+
+            if(response.ok){
+                console.log("User login successful")
+                setErrorResponse("");
+                goTo("/dashboard")
+            }else{
+                console.log("Something went wrong")
+                const json = await response.json();
+                setErrorResponse(json.body.error)
+                return;
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     if (auth.isAuthenticated)    {
         return <Navigate to="/dashboard" />
@@ -15,10 +49,11 @@ export default function LoginForm(){
 
     return (
         <>
-            <a href="" class="a-no-deco logo-login" style={{position: "absolute", padding: 32+"px"}}><FaCube size={50} /><h1>ARTSOLE</h1></a>
+            <a href="" class="a-no-deco logo-login"><FaCube size={50} /><h1>ARTSOLE</h1></a>
             <div class="login-wrapper">
-                <form action="" noValidate>
+                <form action="" noValidate onSubmit={handleSubmit}>
                     <h1>Login</h1>
+                    {!errorResponse && <div class="error-message">{errorResponse}</div>}
                     <div class="text-box">
                         <FaUser size={15}/>
                         <input type="text" placeholder="Usuario" required value={username} onChange={(e) => setUsername(e.target.value)}></input>
